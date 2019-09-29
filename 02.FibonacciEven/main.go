203bae57a8a3e77768a42cab365f6f3f6013ed73
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-09-29 20:14:38 +0100

    Completed challenge 2

--- 02.FibonacciEven/main.go
+++ 02.FibonacciEven/main.go
@@ -4,15 +4,17 @@ import "fmt"
 
 func main() {
 
-	var i, iminus, total int = 1, 0, 0
+	var inext, i, iminus, total int = 1, 1, 0, 0
 
 	for i < 4000000 {
 		if i%2 == 0 {
 			total += i
 		} else {
 		}
-		i += iminus
+		inext = i + iminus
 		iminus = i
+		i = inext
+		fmt.Println("i is ", inext, ", iminus is ", iminus)
 	}
 	fmt.Println(total)
 }
