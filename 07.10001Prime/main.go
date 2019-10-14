db15379d5096637adbc7eb5f967ea43b4a4064c4
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-10-14 06:50:12 +0100

    further advancements done

--- 07.10001Prime/main.go
+++ 07.10001Prime/main.go
@@ -6,13 +6,13 @@ func main() {
 
 	var count, x int = 6, 15 //6th prime is 13, so start counting at 5 and testing at 13
 
-	for i := 2; i < (x/2)+1; i++ { // testing each candidate x, starting by dividing by 2
+	for i := 3; i < (x/3)+1; i++ { // testing each candidate x, starting by dividing by 2
 		if x%i == 0 { //if factor found
 			x += 2 //next x
-			i = 2  // re initialize i
+			i = 3  // re initialize i
 
 		} else { //if factor not found
-			if i == x/2 { //and we are at half, no factor after
+			if i == x/3 { //and we are at half, no factor after
 				count++ //we found another prime, count it
 
 				if count == 10001 { //when 10001 prime is found
@@ -21,13 +21,12 @@ func main() {
 
 				} else { //if not Prime No.10001
 					x += 2 //next x
-					i = 2  // re initialize i
+					i = 3  // re initialize i
 				}
-
 			} else { //next i factor to test
 			}
 		}
 	}
-	fmt.Println("The ", count, "th prime is ", x)
+	fmt.Println("The early ", count, "th prime is ", x)
 	return
 }
