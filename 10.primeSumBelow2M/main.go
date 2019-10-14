b957ef9e56fc1694807ef566c04557bb6e729c2b
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-10-14 21:14:49 +0100

    resolution of the problem 10

--- 10.primeSumBelow2M/main.go
+++ 10.primeSumBelow2M/main.go
@@ -4,39 +4,40 @@ import "fmt"
 
 func main() { //testing the function for all below 1000 first
 
-	primeList := make([]int, 5, 400) //initialize first 5 primes
-	primeList[0] = 1
-	primeList[1] = 2
-	primeList[2] = 3
-	primeList[3] = 5
-	primeList[4] = 7
+	primeList := make([]int, 4, 400000) //initialize first 5 primes
+	primeList[0] = 2
+	primeList[1] = 3
+	primeList[2] = 5
+	primeList[3] = 7
 
 	var x int = 9 //set x to start at next odd after last prime
 
-	for x < 1000 {
+	for x < 2000000 {
 
 		for i := 1; i < len(primeList); i++ { // testing each candidate x, starting by dividing by 1st prime
 
 			if x%primeList[i] == 0 { //factor found
 				x += 2 //next odd x
-				break
+				i = 1
 			} else { //if factor not found
 
-				if primeList[i] == x/3 { //and we are at third, no factor after, this is a Prime
-					primeList = append(primeList, x)
+				if primeList[i] >= x/3 { //and we are at third, no factor after, this is a Prime
+					primeList = append(primeList, x) //add prime to the list
+					fmt.Println(x)
 					x += 2 //next odd x
-					fmt.Println(primeList)
-					break
+					i = 2000000
 				} else { //next i factor to test
 				}
 			}
 		}
 	} //All primes below 2000000 are now in the slice primeList
 
-	var count int                         //create a counter
-	for i := 0; i < len(primeList); i++ { //over the whole lenght of the list
+	var count int                           //create a counter
+	for i := 0; i < len(primeList)-1; i++ { //over the whole lenght of the list
 		count += primeList[i] //sum all the list
 	}
 	fmt.Println("The sum of all Primes below 2M is ", count)
+	fmt.Println("Last x is ", x)
+
 	return
 }
