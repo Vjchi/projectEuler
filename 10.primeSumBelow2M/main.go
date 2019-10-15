913ab4e9fd06901f7032f6ad76b891eedbca4866
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-10-15 06:24:44 +0100

    progressing to the next bit

--- 10.primeSumBelow2M/main.go
+++ 10.primeSumBelow2M/main.go
@@ -18,26 +18,25 @@ func main() { //testing the function for all below 1000 first
 
 			if x%primeList[i] == 0 { //factor found
 				x += 2 //next odd x
-				i = 1
+				break
 			} else { //if factor not found
 
 				if primeList[i] >= x/3 { //and we are at third, no factor after, this is a Prime
 					primeList = append(primeList, x) //add prime to the list
-					fmt.Println(x)
-					x += 2 //next odd x
-					i = 2000000
+					x += 2
+					break //next odd x
 				} else { //next i factor to test
 				}
 			}
 		}
 	} //All primes below 2000000 are now in the slice primeList
 
-	var count int                           //create a counter
-	for i := 0; i < len(primeList)-1; i++ { //over the whole lenght of the list
+	var count int                         //create a counter
+	for i := 0; i < len(primeList); i++ { //over the whole lenght of the list
 		count += primeList[i] //sum all the list
 	}
+
 	fmt.Println("The sum of all Primes below 2M is ", count)
-	fmt.Println("Last x is ", x)
 
 	return
 }
