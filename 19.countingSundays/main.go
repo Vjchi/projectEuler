027045570c70e288b9b671958b634e0a96e3bbcd
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-11-05 21:43:03 +0000

    updated the code

--- 19.countingSundays/main.go
+++ 19.countingSundays/main.go
@@ -3,21 +3,32 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println("Hello World!")
-	fmt.Println(countPastYrs(1952) - countPastYrs(1951))
-
+	fmt.Println(countPastYrs(1900) - countPastYrs(1899))
+	fmt.Println((countPastYrs(2000) + countCurYr(2000, 0)) % 7)
 	return
 }
 
-func countCurYr(yr, month, day int) int {
+func countCurYr(yr, month int) int {
 	var x int
-
+	x = daysPastMonth[month]
+	if month > 1 {
+		if yr%400 == 0 {
+			x++
+			return x
+		}
+		if yr%100 == 0 {
+			return x
+		}
+		if yr%4 == 0 {
+			x++
+			return x
+		}
+	}
 	return x
 }
 
 func countPastYrs(yr int) int {
-	y := (yr)*365 + yr/4 - yr/100 + yr/400
+	y := (yr)*365 + (yr)/4 - (yr)/100 + (yr)/400
 	y -= dayOne
 	return y
 }
