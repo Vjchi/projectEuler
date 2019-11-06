a440ec874fb4a52441e34b8d1c2beb22f76c9384
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-11-06 07:14:04 +0000

    added description

--- 19.countingSundays/main.go
+++ 19.countingSundays/main.go
@@ -3,32 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(countPastYrs(1900) - countPastYrs(1899))
-	fmt.Println((countPastYrs(2000) + countCurYr(2000, 0)) % 7)
+
+	for i := 0; i < 12; i++ {
+		fmt.Println(i, " - ", (countPastYrs(1960)+countCurYr(1960, i))%7)
+	}
+	/*var counter int
+	for i := 1901; i <= 2001; i++ {
+		pastYr := countPastYrs(i)
+		for j := 0; j < 12; j++ {
+			if (countCurYr(i, j)+pastYr)%7 == 0 {
+				counter++
+			}
+		}
+	}
+	fmt.Println(counter)*/
 	return
 }
 
 func countCurYr(yr, month int) int {
-	var x int
+	var x int //set the day counter
 	x = daysPastMonth[month]
-	if month > 1 {
-		if yr%400 == 0 {
+	if month > 1 { //if we are after February
+		if yr%400 == 0 { //and the yr is multiple of 400, add 29/2
 			x++
-			return x
+			return x //and return the number of days
 		}
-		if yr%100 == 0 {
-			return x
+		if yr%100 == 0 { //but if century, don't add 29/2
+			return x //and return the number of days
 		}
-		if yr%4 == 0 {
+		if yr%4 == 0 { //but if still multiple of 4, add 29/2
 			x++
-			return x
+			return x //and return the number of days
 		}
 	}
 	return x
 }
 
 func countPastYrs(yr int) int {
-	y := (yr)*365 + (yr)/4 - (yr)/100 + (yr)/400
+	y := yr*365 + yr/4 - yr/100 + yr/400
 	y -= dayOne
 	return y
 }
