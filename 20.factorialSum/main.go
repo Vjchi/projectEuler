043b972d1df43eb2a51e639f680aeab9be75a1aa
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-11-10 20:08:44 +0000

    no.20 solved

--- 20.factorialSum/main.go
+++ 20.factorialSum/main.go
@@ -29,20 +29,20 @@ func main() {
 		}
 	}
 	fmt.Println(primeSlA)
-	var numCnt []int = make([]int, 130, 130) //set our digit list
+	var numCnt []int = make([]int, 200, 200) //set our digit list
 	numCnt[0] = 1                            //initialise it
 
 	for i := range primeSlA {
-		var remCnt []int = make([]int, 132, 132) //set our remainder list
+		var remCnt []int = make([]int, 203, 203) //set our remainder list
 		for j := range numCnt {
 			numCnt[j] = numCnt[j] * primeSlA[i] //multiply each digit by the prime
-			remCnt[j+2] = numCnt[j] / 100       //Store remainder >100
+			remCnt[j+2] += numCnt[j] / 100      //Store remainder >100
 			numCnt[j] = numCnt[j] % 100         //remove remainder part >100
-			remCnt[j+1] = numCnt[j] / 10        //Store remainder >10
+			remCnt[j+1] += numCnt[j] / 10       //Store remainder >10
 			numCnt[j] = numCnt[j] % 10          //Only keep unit digit
 		} //next j
 
-		for j := 0; j < len(numCnt); j++ { //now to associate remainder to counter
+		for j := range numCnt { //now to associate remainder to counter
 			numCnt[j] += remCnt[j] //add the remainder to our digit counter
 		} //next j
 
@@ -57,7 +57,7 @@ func main() {
 	for i := range numCnt {
 		total += numCnt[i]
 	}
-	fmt.Println(numCnt[0:130])
+	fmt.Println(numCnt[0:200]) //print the complete number, in the wrong order
 	fmt.Println(total)
 
 	return
