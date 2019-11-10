30168a029656fca3860e7d65c33940e100de5f5f
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-11-10 18:07:21 +0000

    new try

--- 20.factorialSum/main.go
+++ 20.factorialSum/main.go
@@ -31,17 +31,39 @@ func main() {
 	}
 	fmt.Println(primeSlA)
 
-	var numCnt []int = make([]int, 5000, 5000)
-	var remCnt []int = make([]int, 5000, 5000)
-	numCnt[0], remCnt[0] = 1, 0
+	var numCnt []int = make([]int, 500, 500) //set our digit list
+
+	numCnt[0] = 1 //initialise it
+
 	for i := range primeSlA {
+
+		var remCnt []int = make([]int, 502, 502) //set our remainder list
+
 		for j := range numCnt {
-			numCnt[j] = numCnt[j] * primeSlA[i]
-			remCnt[j+2] = numCnt[j] / 100
-			remCnt[j+1] = numCnt[j] / 10
-			numCnt[j] = numCnt[j] % 10
+			numCnt[j] = numCnt[j] * primeSlA[i] //multiply each digit by the prime
+			remCnt[j+2] = numCnt[j] / 100       //Store remainder >10
+			numCnt[j] = numCnt[j] % 100         //remove remainder part >100
+			remCnt[j+1] = numCnt[j] / 10        //Store remainder >10
+			numCnt[j] = numCnt[j] % 10          //Only keep unit digit
 		}
+
+		for j := 0; j < 500; j++ { //now to associate remainder to counter
+			numCnt[j] += remCnt[j] //add the remainder to our digit counter
+		}
+
+		for j := 0; j < 498; j++ { //now to rearrange counter
+			numCnt[j+2] += numCnt[j] / 100 //add count >100
+			numCnt[j] = numCnt[j] % 100    //remove count part >100
+			numCnt[j+1] += numCnt[j] / 10  //add count >10
+			numCnt[j] = numCnt[j] % 10     //only keep singl digit
+		}
+	} //by the end of it, we should have all the digits making 100!
+	var total int
+	for i := range numCnt {
+		total += numCnt[i]
 	}
+
+	fmt.Println(total)
 
 	return
 }
