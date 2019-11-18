e222dc56bbb49bad0f07846b0490d81d7fdee882
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-11-18 20:50:35 +0000

    completed challenge 21 :)

--- 21.amicableNumbers/main.go
+++ 21.amicableNumbers/main.go
@@ -12,19 +12,31 @@ type primeFac struct {
 
 func main() {
 	var primeList []int //Set primeList variable
-	fmt.Println("Enter the lenght you'd like to study:")
-	var x int                   //set variable to study
-	fmt.Scan(&x)                //Get variable from user
-	primeList = setPrimeList(x) //create prime List from variable
-	sumDiv(x, primeList)
+	var count int
+	primeList = setPrimeList(10000) //create prime List from variable
+
+	//loop over the calculatiom for the sum of divisors
+	for i := 5; i < 10000; i++ {
+		var a, b int
+		a = sumDiv(i, primeList)
+		b = sumDiv(a, primeList)
+		if i == b && a != i {
+			count += i
+		}
+	}
+	fmt.Println("count is:", count)
 	return
 }
 
-func sumDiv(a int, primeList []int) {
+func sumDiv(a int, primeList []int) int {
+	//first, cover the out of scope posibilities
+	if a == 1 || a > 10000 {
+		return 0
+	}
 	var primeFacList = make([]primeFac, 0, len(primeList)) //create slice
 	i, t := a, 0
 
-	for i != 1 { //for each prime divider, as long as alias != 1
+	for i != 1 { //for each prime divider, as long as alias != 1 AND dividor lower than third
 		if i%primeList[t] == 0 { //as long as prime is a divider
 			primeFacList = append(primeFacList, primeFac{primeList[t], 1}) //initialize the prime counter
 			i = i / primeList[t]                                           //divide the alias
@@ -37,45 +49,43 @@ func sumDiv(a int, primeList []int) {
 		}
 	} //until we get to t = 1
 	//primeFacList now holds all the prime divisors and their factors
-	fmt.Println(primeFacList)
 
 	divLenght := 1
 	for i := range primeFacList {
 		divLenght = divLenght * (primeFacList[i].exp + 1)
 	}
-	fmt.Println(divLenght)
+
 	var countList []float64 = make([]float64, divLenght) //create the count list
 	for i := range countList {
 		countList[i] = 1 //Inialize the count list
 	}
-
-	//-----------------------------SET FIRST FACTOR
-
+	//SET FIRST FACTOR
 	var set1 = 1
 
 	for i := range countList {
 		countList[i] = math.Pow(float64(primeFacList[0].prime), float64((i % (divLenght / set1) / (divLenght / (primeFacList[0].exp + 1)))))
 	}
 	set1 *= primeFacList[0].exp + 1
-	fmt.Println(countList)
 
-	//---------------------------- COMPLETE THE LIST
-
+	//COMPLETE THE LIST
 	for i := 1; i < len(primeFacList); i++ {
 
 		set1 *= (primeFacList[i].exp + 1)
-		fmt.Println("Set1 is ", set1)
-
 		lscope := divLenght / set1
 
 		for j := range countList {
 			countList[j] *= math.Pow(float64(primeFacList[i].prime), float64((j/lscope)%(primeFacList[i].exp+1)))
 		}
-
-		fmt.Println(countList)
 	}
-	fmt.Println("Final", countList)
-	return
+	var count float64
+	var cntInt int
+
+	for i := range countList {
+		count += countList[i]
+	}
+	cntInt = int(count) - a
+
+	return cntInt
 }
 
 //-----------------------------------------------------
@@ -86,8 +96,8 @@ func setPrimeList(a int) []int {
 	primeList[0] = 2                    //put first prime in
 	primeList[1] = 3                    //put second prime in
 	var x int = 5                       //set x to start at next odd after last prime
-	for x < a {
-		for i := 1; i < len(primeList); i++ { // testing each candidate x, starting by dividing by 1st prime
+	for x <= a {
+		for i := 0; i < len(primeList); i++ { // testing each candidate x, starting by dividing by 1st prime
 			if x%primeList[i] == 0 { //factor found
 				x += 2 //next odd x
 				break
