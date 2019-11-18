21cefb2631497414b656a450c9c4fbe39eac4b7b
vjchi <31632164+Vjchi@users.noreply.github.com>
2019-11-18 21:55:39 +0000

    progress 22

--- 22.namesScores/main.go
+++ 22.namesScores/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
 
 func main() {
 
 	fmt.Println("Hello World")
+	byteNames, err := ioutil.ReadFile("D:/Documents/GoWorplace/src/projectEuler/22.namesScores/p022_names.txt")
+	if err != nil { //log if error
+		log.Fatal(err)
+	}
+
+	var strNames string = string(byteNames)                 //convert raw byte in string
+	var sliStrNames []string = strings.Split(strNames, ",") //split by comma
+
+	for i := range sliStrNames {
+		sliStrNames[i] = strings.Trim(sliStrNames[i], "\"")
+	}
+
+	fmt.Println(sliStrNames)
 
 	return
 }
